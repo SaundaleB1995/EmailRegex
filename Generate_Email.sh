aad97d825bf3870ceb51b9a81bccd7c447378b65
Bhushan Saundale <saundalebhushan5@gmail.com>
2020-03-29 23:40:11 +0530

    [BHUSHAN]Added:Generating the Email Pattern file.

--- Generate_Email.sh
+++ Generate_Email.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
    echo "Welcome to the Email Pattern."
+
+     shopt -s extglob
+
+        echo "Enter the Email:"
+        read email
+
+emailPattern="^[a-z]{3,}([.][a-z]+)[@][a-z]+[.][a-z]{2}"
+
+        if [[ $email =~ $emailPattern ]];
+        then
+                echo CORRECT;
+        else
+                echo FAIL;
+        fi
+
